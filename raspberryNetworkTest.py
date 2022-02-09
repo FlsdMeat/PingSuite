692da2d16da15cbf20e59fce7f74b2efe74316f1
FlsdMeat <colinhollister123@gmail.com>
2022-02-09 13:46:07 -0500

    Added STDDEV for ping test and started sql file

--- raspberryNetworkTest.py
+++ raspberryNetworkTest.py
@@ -7,7 +7,6 @@ import smtplib
 from getmac import get_mac_address as gma
 from email.message import EmailMessage
 import datetime
-from math import sqrt
 
 
 def pingTest(results):
@@ -53,23 +52,6 @@ def speedTest(results):
         print(json.decoder.JSONDecodeError, obj, s.results.dict())
         results.send(str(s.results.dict()))
 
-def emailResults(results):
-    #Grab time, hostname of device, ip and mac. Setup a new email object
-    current_time = datetime.datetime.now()
-    hostname = socket.gethostname()   
-    IPAddr = socket.gethostbyname(hostname)
-    msg = EmailMessage()
-    msg['Subject'] = ("Python Ping Test " + str(current_time) + " from MAC " + gma())
-    print("Got mac")
-    msg['From'] = 'Automated Email System written by Colin H, OIT Echlin'
-    msg['To'] = 'colinhollister123@gmail.com'
-    msg.set_content(
-        'Hostname: ' + hostname + '\nIP Address: ' + IPAddr + '\nMac Address: ' + gma() + '\nSpeedtest results:\n' +
-        results[0] + '\nPing Test Results:\n' + results[1]
-    )
-    print("MSG Ready")
-    print(msg)
-
 if __name__ == '__main__':
     #setup a new pipe for 2 shared objects
     pingResults, speedResults = Pipe()
@@ -85,4 +67,3 @@ if __name__ == '__main__':
     results = [pingResults.recv(),speedResults.recv()]
     #print in console and start email
     print(results[0], results[1])
-    emailResults(results)
