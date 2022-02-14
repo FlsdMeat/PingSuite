25160717e9d9bdb88478d69900a35f2eb14462b1
FlsdMeat <colinhollister123@gmail.com>
2022-02-14 17:11:33 -0500

    Added ping execution on wireless adapter

--- raspberryNetworkTest.py
+++ raspberryNetworkTest.py
@@ -1,6 +1,7 @@
 import speedtest
 from pythonping import ping
 from multiprocessing import Process, Pipe
+import subprocess
 import json
 import socket
 import smtplib
@@ -8,24 +9,36 @@ from email.message import EmailMessage
 import datetime
 from math import sqrt
 from database import startDatabaseWorker
+import psutil
+import os
+
+addrs = psutil.net_if_addrs()
+ipAdd = '127.0.0.1'
+for netDev in addrs:
+    temp = addrs[netDev][0][1]
+    if("172.26" in temp):
+        ipAdd = netDev
 
 def pingTest(results):
+    pingResults = subprocess.Popen(['ping', '-I' + str(ipAdd), '-i 0.5', '-c 100', '8.8.8.8'], stdout=subprocess.PIPE)
+    output = str(pingResults.communicate()).replace('n64 bytes from 8.8.8.8: ', '').split('\n')
+    output = output[0].split('\\')
+    for item in range(1, len(output)-5):
+        output[item] = output[item][output[item].index('time=')+5:-3]
+    output.pop(0)
+    output.pop(-1)
     #Runs a ping test, converts data into a string. 100 pings, 50 seconds totalpingResults = str(ping('8.8.8.8', verbose=False, count=5,interval=0.5))
-    pingResults = str(ping('8.8.8.8', count=5,interval=0.5))
-    pingResults = pingResults.replace('Reply from 8.8.8.8, ', '')
-    resultsArray = pingResults.split('\n')
-    currentPingStats = resultsArray[-1][32:-2].split('/')
-    for x in range(len(currentPingStats)):
-        currentPingStats[x] = float(currentPingStats[x])
-    currentPingStats = {'min': currentPingStats[0], 'avg':currentPingStats[1], 'max':currentPingStats[2]}
-    resultsArray = resultsArray[:-2]
+    currentPingStats = str(output[-1][output[-1].index('= ')+2:-3]).split('/')
+    percentLoss = str(output[-2][output[-2].index('%') - 1:output[-2].index('%')])
+    currentPingStats = {'loss':percentLoss +'%','min': currentPingStats[0], 'avg':currentPingStats[1], 'max':currentPingStats[2], 'mdev':currentPingStats[3]}
+    resultsArray = output[:-4]
     sumOfResults = 0
     deviation = [0 for i in range(len(resultsArray))]
     sumOfdeviation = 0
     for x in range(len(resultsArray)):
-        resultsArray[x] = float(resultsArray[x][12:-3])
+        resultsArray[x] = float(resultsArray[x])
         sumOfResults += resultsArray[x]
-        deviation[x] = (resultsArray[x] - currentPingStats['avg']) ** 2
+        deviation[x] = (resultsArray[x] - float(currentPingStats['avg'])) ** 2
         sumOfdeviation += deviation[x]
     standardDev = sqrt(sumOfdeviation/len(resultsArray))
     currentPingStats['stddev'] = standardDev
@@ -66,4 +79,5 @@ if __name__ == '__main__':
     speedtestThread.join()
     #grab results
     results = [pingResults.recv(),speedResults.recv()]
-    startDatabaseWorker(results[0], results[1])
+    print(results)
+    #startDatabaseWorker(results[0], results[1])
--- test.py
+++ test.py
@@ -1,21 +1,9 @@
-from pythonping import ping
-from math import sqrt
-pingResults = str(ping('8.8.8.8', verbose=False, count=5,interval=0.5))
-pingResults = pingResults.replace('Reply from 8.8.8.8, ', '')
-resultsArray = pingResults.split('\n')
-currentPingStats = resultsArray[-1][32:-2].split('/')
-for x in range(len(currentPingStats)):
-    currentPingStats[x] = float(currentPingStats[x])
-currentPingStats = {'min': currentPingStats[0], 'avg':currentPingStats[1], 'max':currentPingStats[2]}
-resultsArray = resultsArray[:-2]
-sumOfResults = 0
-deviation = [0 for i in range(len(resultsArray))]
-sumOfdeviation = 0
-for x in range(len(resultsArray)):
-    resultsArray[x] = float(resultsArray[x][12:-3])
-    sumOfResults += resultsArray[x]
-    deviation[x] = (resultsArray[x] - currentPingStats['avg']) ** 2
-    sumOfdeviation += deviation[x]
-standardDev = sqrt(sumOfdeviation/len(resultsArray))
-currentPingStats['stddev'] = standardDev
-print(currentPingStats)
+import psutil
+
+addrs = psutil.net_if_addrs()
+for netDev in addrs:
+    ipAdd = addrs[netDev]
+    ipAdd = ipAdd[0]
+    ipAdd = ipAdd[1]
+    if("172.26" in ipAdd):
+        print(ipAdd)
