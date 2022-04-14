15f3fc1ab27926801f83689cd4d9ab922e27a28d
FlsdMeat <colinhollister123@gmail.com>
2022-04-14 09:56:03 -0400

    Moving around functions to organize

--- nodeJS/JSONAPI/pingResults.js
+++ nodeJS/JSONAPI/pingResults.js
@@ -1,59 +1,11 @@
-//Express is used for the api aspect of NodeJS
-const express = require('express');
-const app = express()
-const server = require('http').createServer(app);
-//Database Functions
-const { uploadSpeedTest, getPingResults,getCurrentDeviceDetails } = require('./database.js')
-//Logging for the app posting
-const { appPostLog } = require('./logging.js')
+
 //Api for creating the graphs
-const { GraphAPI } = require('./GraphAPI/GraphAPI.js')
-require('dotenv').config()
+const { GraphAPI } = require('../GraphAPI/GraphAPI.js')
+//Database Functions
+const { getPingResults, getCurrentDeviceDetails } = require('../MariaDB/database.js')
+const { appPostLog } = require('../logs/logging.js')
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-let cache = {}
-
-app.post('/pingResults', (req,res) => {
-    let data = req.body
-    try {
-        Object.keys(data).forEach(item=>{
-            if(item == 'SpeedTest' || item == 'PingResults'){
-                data[item] = JSON.parse(data[item])
-            }
-        })
-        sendToDatabase(data)
-        appPostLog(`${data.name} with a mac ${data.mac} just sent a ping!`)
-        return res.send(true)
-    } catch (error) {
-        appPostLog(`[AppPost pingResults]: Error with parsing JSON`,error)
-        return res.send(false)
-    }
-})
-const getPrefix = (num) => {
-    let temp = (num.toString()).substring((num.toString()).length - 1)
-    if ((num.toString())[0] === '0'){
-        if (temp === '1'){
-        return temp + 'st'
-        } else if (temp === '2'){
-        return temp + 'nd'
-        } else if (temp === '3'){
-        return temp + 'rd'
-        }
-        return temp + 'th'
-    } else {
-        if (temp === '1'){
-        return num + 'st'
-        } else if (temp === '2'){
-        return num + 'nd'
-        } else if (temp === '3'){
-        return num + 'rd'
-        }
-        return num + 'th'
-    }
-}
-app.get('/api/pingResults/*', async (req, res) => {
+async function PingResults(req, res, cache){
     let ipLocal = {
         1:'Harugari Hall',2:'South Campus Hall',3:'1124 Campbell Ave',4:'Kaplan Hall',5:'Dodds Hall', 6:'Bethel Hall',7:'Gate House', 8:'Campus Store / UNH PD', 9:'Bartels Hall', 10:'Buckman Hall / Utility Building',
         11:'Peterson Library', 12:'Maxcy Hall', 13:'Bayer Hall', 14:'Bartels Student Activity Center', 15:'Dunham Hall', 16:'Sheffield Hall', 17:'Winchester Hall', 18:'Arbeiter Maenner Chor', 19:'North Campus - House', 20:'Echlin Hall'
@@ -161,32 +113,6 @@ app.get('/api/pingResults/*', async (req, res) => {
         appPostLog(`[AppGet pingResults]`,error)
         return res.json(false)
     }
-})
-const resetCache = () =>{
-    setTimeout(() => {
-        try {
-            Object.keys(cache).forEach(item=>{
-                delete cache[item]
-            })
-        } catch (error) {}
-        resetCache()
-    }, process.env.CACHE_RESET);
-}
-resetCache()
-
-function sendToDatabase(data){
-    let speedTest = data.SpeedTest
-    let pingTest = data.PingResults
-    let dataResults = uploadSpeedTest(speedTest, pingTest, data.mac, data.name, data.ip)
 }
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(__dirname + '/web-portal/build'));
-      
-    // Handle React routing, return all requests to React app
-    app.get('/', function(req, res) {
-      res.sendFile(__dirname + '/web-portal/build/' + 'index.html');
-    });
-}
-server.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
+module.exports = {PingResults}
